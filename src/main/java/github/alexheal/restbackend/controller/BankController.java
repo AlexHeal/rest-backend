c93a70d94581b193ec7fcd047e517bac1e20c95c
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-05-08 14:54:44 +0500

    ADD: getAllUsersInfo

--- src/main/java/github/alexheal/restbackend/controller/BankController.java
+++ src/main/java/github/alexheal/restbackend/controller/BankController.java
@@ -3,14 +3,23 @@ package github.alexheal.restbackend.controller;
 import github.alexheal.restbackend.domain.LoginInfo;
 import github.alexheal.restbackend.domain.UserInfo;
 import github.alexheal.restbackend.exception.InvalidUserNameException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class BankController {
+    Map<String, UserInfo> users = Map.of(
+            "ALex", UserInfo.builder().username("Alex").build(),
+            "Olga", UserInfo.builder().username("Olga").build(),
+            "Invan", UserInfo.builder().username("Invan").build()
+    );
 
     @PostMapping("user/login")
     public UserInfo login(@RequestBody LoginInfo loginInfo) {
@@ -22,5 +31,13 @@ public class BankController {
         } else {
             throw new InvalidUserNameException();
         }
+    }
+
+    @GetMapping("user/getAll")
+    public List<UserInfo> getAllUsersInfo() {
+        return users.entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
     }
 }
